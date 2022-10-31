3d2b90e459f873b95c21f62f3490387e4ab8dc0b
thiagohenriquedelimajaques@gmail.com <thiagohenriquedelimajaques@gmail.com>
2022-10-31 17:36:32 -0300

    Instalado servidor de integração continua

--- projetodjango/urls.py
+++ projetodjango/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from projetodjango.home_view import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',home),
 ]
